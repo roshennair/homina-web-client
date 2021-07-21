7ca70067e07bfaabbc6d67873a747997116ed24d
Roshen Nair <roshen620@gmail.com>
2021-07-21 22:23:00 +0800

    Move SignUp component to new pages directory

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import SignUp from './components/SignUp';
+import SignUp from './pages/SignUp';
 
 const App = () => {
-    return (
-        <Router>
-            <Route path='/'>
-                <Redirect to='/sign-up' />
-            </Route>
-            <Route path='/sign-up'>
-                <SignUp />
-            </Route>
-        </Router>
-    );
+	return (
+		<Router>
+			<Route path='/'>
+				<Redirect to='/sign-up' />
+			</Route>
+			<Route path='/sign-up'>
+				<SignUp />
+			</Route>
+		</Router>
+	);
 }
 
 export default App;
