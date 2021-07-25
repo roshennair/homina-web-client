54cb9b8aeecaa820fc342fea4472e3295d754366
Roshen Nair <roshen620@gmail.com>
2021-07-25 23:33:32 +0800

    Refactor signUp and login functions, implement logout function

--- src/contexts/authContext.jsx
+++ src/contexts/authContext.jsx
@@ -12,45 +12,59 @@ export const useAuth = () => useContext(AuthContext);
 export const AuthProvider = ({ children }) => {
 	const [currentUser, setCurrentUser] = useState(null);
 
-	const signUp = async (name, email, username, password) => {
-		try {
-			const response = await fetch(`http://${SERVER_HOST}:${SERVER_PORT}/signup`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({ name, email, username, password })
-			});
+	const signUp = async (signUpData) => {
+		const response = await fetch(`http://${SERVER_HOST}:${SERVER_PORT}/signup`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(signUpData)
+		});
 
-			if (!response.ok) throw new Error(response.body);
-
+		if (response.ok) {
 			const user = await response.json();
 			setCurrentUser(user);
-		} catch (error) {
-			throw new Error(error);
+		} else {
+			const error = await response.json();
+			throw error;
 		}
 	}
 
-	const login = async (username, password) => {
-		try {
-			const response = await fetch(`http://${SERVER_HOST}:${SERVER_PORT}/login`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({ username, password })
-			});
+	const login = async (loginCreds) => {
+		const response = await fetch(`http://${SERVER_HOST}:${SERVER_PORT}/login`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(loginCreds)
+		});
 
-			if (!response.ok) throw new Error(response.body);
-
+		if (response.ok) {
 			const user = await response.json();
 			setCurrentUser(user);
-		} catch (error) {
-			throw new Error(error);
+		} else {
+			const error = await response.json();
+			throw error;
 		}
 	}
 
-	const providerValue = { currentUser, signUp, login };
+	const logout = async () => {
+		const response = await fetch(`http://${SERVER_HOST}:${SERVER_PORT}/logout`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+
+		if (response.ok) {
+			setCurrentUser(null);
+		} else {
+			const error = await response.json();
+			throw error;
+		}
+	}
+
+	const providerValue = { currentUser, signUp, login, logout };
 
 	return (
 		<AuthContext.Provider value={providerValue}>
